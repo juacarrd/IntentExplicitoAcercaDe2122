6f2bb701e55e79b120af889c391c4b00e6a06718
juacarr <juacarrd@v-espino.es>
2021-11-03 16:50:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/intentexplicito/AcercaDeActivity.java
@@ -0,0 +1,16 @@
+package com.example.intentexplicito;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AcercaDeActivity extends Activity {
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
+
+
